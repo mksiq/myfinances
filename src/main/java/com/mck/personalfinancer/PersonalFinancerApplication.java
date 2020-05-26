a38d138ae07c8811693a4ceca2e2cfc4c2954b75
mksiq <maickelsiqueira@gmail.com>
2020-05-26 16:08:03 -0400

    empty

--- src/main/java/com/mck/personalfinancer/PersonalFinancerApplication.java
+++ src/main/java/com/mck/personalfinancer/PersonalFinancerApplication.java
@@ -2,10 +2,19 @@ package com.mck.personalfinancer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class PersonalFinancerApplication {
+@EnableWebMvc
+public class PersonalFinancerApplication implements WebMvcConfigurer{
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET","PUT", "POST", "DELETE", "OPTIONS");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalFinancerApplication.class, args);
 	}
